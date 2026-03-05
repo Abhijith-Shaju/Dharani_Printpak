8c5137a3ead228b6b2107a87a2b6a5b23eb75393
Visvatharsan <visvatharsan6@gmail.com>
2026-03-05 23:32:54 +0530

    orange bg mobile fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   content: ["./index.html", "./scripts/**/*.js"],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "769px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {
       animation: {
         testspin: "testspin 5s linear infinite",
